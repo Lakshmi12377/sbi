e148ca6b3daa20597dd49bd900942c669c1f6835
Lakshmi12377 <peralilakshmi@gmail.com>
2020-07-06 18:29:55 +0530

    coding moving

---
+++ CoreJavaExamples/src/com/corejava/commmanline/CommandLineArgumentExample1.java
@@ -0,0 +1,11 @@
+package com.corejava.commmanline;
+
+public class CommandLineArgumentExample1 {
+
+	public static void main(String args[]) {
+		//System.out.println("Enter value: ");
+		//for(int i=0;i<args.length;i++)
+		System.out.println(args[0]+args[1]);
+	}
+
+}
